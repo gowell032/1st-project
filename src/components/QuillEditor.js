382350d780026b986c0314611ea5bccb67358cc7
gowell032 <gowell032@gmail.com>
2023-05-23 12:26:10 +0900

    base64 -> url 변환 함수 완료

---
+++ src/components/QuillEditor.js
@@ -0,0 +1,132 @@
+import axios from 'axios';
+import React, { useRef, useState } from 'react';
+import { useMemo } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import styled from 'styled-components';
+
+function QuillEditor({ value, onChange }) {
+  const quillRef = useRef(null);
+  const token = localStorage.getItem('token');
+  const [gotUrl, setGotUrl] = useState([]);
+
+  const contentHandler = (content) => {
+    onChange(content);
+  };
+
+  // 이미지 처리 함수
+
+  const imageHandler = () => {
+    const input = document.createElement('input');
+    const formData = new FormData();
+    let url = '';
+
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+
+    input.onchange = () => {
+      const file = input.files;
+      if (file !== null) {
+        formData.append('image', file[0]);
+        axios
+          .post('/community/post/image', formData, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+          .then((response) => {
+            url = response.data;
+            let urlArr = gotUrl;
+            urlArr.push(url);
+            setGotUrl(urlArr);
+            const range = quillRef.current?.getEditor().getSelection()?.index;
+            if (range !== null && range !== undefined) {
+              let quill = quillRef.current?.getEditor();
+
+              quill?.setSelection(range, 1);
+
+              quill?.clipboard.dangerouslyPasteHTML(
+                range,
+                `<img src=${url} alt="article image" />`
+              );
+            }
+          })
+          .catch((error) => alert(error));
+      } else {
+        alert('5MB 이하의 이미지만 업로드할 수 있습니다.');
+      }
+    };
+  };
+
+  // modules에 useMemo를 써주지 않으면 콘솔창에
+  // addrange() the given range isn't in document 에러 발생
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          [{ header: '1' }, { header: '2' }],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          ['image'],
+        ],
+        handlers: { image: imageHandler },
+      },
+    };
+  }, []);
+
+  const formats = [
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'link',
+    'image',
+  ];
+
+  return (
+    <div>
+      <TextEditor
+        ref={quillRef}
+        theme="snow"
+        placeholder={'내용을 작성하세요'}
+        value={value}
+        onChange={contentHandler}
+        modules={modules}
+        formats={formats}
+      />
+    </div>
+  );
+}
+
+const TextEditor = styled(ReactQuill)`
+  .ql-container {
+    height: 500px;
+    .ql-editor.ql-blank::before {
+      font-size: 16px;
+    }
+    .ql-editor strong {
+      font-weight: bold;
+    }
+    .ql-editor em {
+      font-style: italic;
+    }
+    h1 {
+      font-size: 32px;
+    }
+    h2 {
+      font-size: 24px;
+    }
+    p {
+      font-size: 16px;
+    }
+  }
+`;
+
+export default QuillEditor;
--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,20 +1,19 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
 import styled from 'styled-components';
+import QuillEditor from '../components/QuillEditor';
 
 function Write() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [subCategoryId, setSubCategoryId] = useState(0);
 
-  const contentHandler = (content) => {
-    setContent(content);
-  };
-
   const titleHandler = (e) => {
     setTitle(e.target.value);
+  };
+  const newContentHandler = (newContent) => {
+    console.log(newContent);
+    setContent(newContent);
   };
 
   const domesticHandler = (e) => {
@@ -36,6 +35,13 @@ function Write() {
 
   const writePost = (e) => {
     e.preventDefault();
+    if (body.title === '') {
+      alert('제목을 입력해주세요.');
+    } else if (body.subCategoryId === 0) {
+      alert('카테고리를 선택해주세요.');
+    } else if (body.content === '') {
+      alert('컨텐츠를 입력해주세요.');
+    }
     axios
       .post('/community/post', body, {
         headers: {
@@ -51,34 +57,6 @@ function Write() {
       })
       .catch((error) => console.log(error));
   };
-  const modules = {
-    toolbar: [
-      [{ header: 1 }, { header: 2 }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [
-        { list: 'ordered' },
-        { list: 'bullet' },
-        { indent: '-1' },
-        { indent: '+1' },
-      ],
-      ['link', 'image'],
-      ['clean'],
-    ],
-  };
-
-  const formats = [
-    'header',
-    'bold',
-    'italic',
-    'underline',
-    'strike',
-    'blockquote',
-    'list',
-    'bullet',
-    'indent',
-    'link',
-    'image',
-  ];
 
   return (
     <Container>
@@ -97,14 +75,7 @@ function Write() {
             국외
           </button>
         </CategoriesWrap>
-        <TextEditor
-          theme="snow"
-          placeholder={'내용을 작성하세요'}
-          value={content}
-          onChange={contentHandler}
-          modules={modules}
-          formats={formats}
-        />
+        <QuillEditor value={content} onChange={newContentHandler} />
         <BtnBox>
           <button>돌아가기</button>
           <button onClick={writePost}>게시하기</button>
@@ -117,14 +88,6 @@ function Write() {
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: linear-gradient(
-    to right top,
-    #f4f5f9,
-    #f7f7fa,
-    #fafafc,
-    #fcfcfd,
-    #ffffff
-  );
   form {
     margin: 50px auto;
     max-width: 800px;
@@ -141,59 +104,32 @@ const Container = styled.div`
     border-right: none;
     border-bottom: 2px solid #cccccc;
     outline: none;
-    background-image: linear-gradient(
-      to right top,
-      #f4f5f9,
-      #f7f7fa,
-      #fafafc,
-      #fcfcfd,
-      #ffffff
-    );
   }
   input:focus {
-    border-bottom: 2px solid #2bc194;
+    border-bottom: 2px solid #303030;
   }
 `;
 
 const CategoriesWrap = styled.div`
   button {
-    width: 50px;
-    height: 30px;
-  }
-`;
-const TextEditor = styled(ReactQuill)`
-  .ql-container {
-    height: 500px;
-    .ql-editor.ql-blank::before {
-      font-size: 16px;
-    }
-    .ql-editor strong {
-      font-weight: bold;
-    }
-    .ql-editor em {
-      font-style: italic;
-    }
-    h1 {
-      font-size: 32px;
-    }
-    h2 {
-      font-size: 24px;
-    }
-    p {
-      font-size: 16px;
-    }
+    margin: 10px 5px 10px 0px;
+    width: 60px;
+    height: 40px;
+    background-color: #303030;
+    color: #faf8ff;
+    border: none;
+    border-radius: 15px;
   }
 `;
 const BtnBox = styled.div`
   align-self: flex-end;
   button {
-    margin-top: 10px;
-    margin-left: 5px;
+    margin: 10px 0px 0px 5px;
     width: 100px;
     height: 42px;
     border: none;
     border-radius: 15px;
-    background-color: #2bc194;
+    background-color: #303030;
     color: #faf8ff;
   }
 `;
